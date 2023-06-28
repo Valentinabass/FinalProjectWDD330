df2fa053d85263efd786e577ebf6a93e5a600eec
Valentinabass <bas21003@byui.edu>
2023-06-28 20:02:06 -0300

    localstorage

--- src/js/dictionary.js
+++ src/js/dictionary.js
@@ -2,7 +2,7 @@ const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const result = document.getElementById("result");
 const sound = document.getElementById("sound");
 const btn = document.getElementById("search-btn");
-const favorite = document.getElementById("add-btn");
+const addBtn = document.getElementById("add-btn");
 
 btn.addEventListener("click", () => {
     let inpWord = document.getElementById("inp-word").value;
@@ -36,18 +36,56 @@ function playSound() {
     sound.play();
 
 }
-//  add to fav 
 
-// function getLocalStorage(key) {
-//     return JSON.parse(localStorage.getItem(key));
-//   }
-  
-//   // save data to local storage
-// function setLocalStorage(key, data) {
-//     localStorage.setItem(key, JSON.stringify(data));
-//   }
-  
-// favorite.addEventListener("click", () => {
-//     let  word = document.getElementById("inp-word").value;
-//     let wordMeaning = document.getElementById("word-meaning").value;
-//     });
+    //BOX FAVORITE LIST!
+
+//     // <button id="remove-btn"><i class="fa-solid fa-xmark"></i></button>
+//     const word = document.getElementById("inp-word");
+//     const btnAdd = document.getElementById("add-btn");
+//     const containerTwo = document.querySelector(".container-two");
+//     // let id= 1;
+
+// function addAll() {
+//     const div_todo = document.createElement("div");
+//     const div_container = document.createElement("div");
+//     const parrafo  = document.createElement("p");
+//     // const div_boton = document.createElement("div");
+//     // const button = document.createElement("button");
+//     // const icono = document.createElement("i");
+
+
+//     parrafo.innerHTML = word.value;
+//     div_todo.classList.add("container-list-first");
+//     div_container.classList.add("container-list");
+//     parrafo.classList.add("added-word");
+//     // div_boton.classList.add("div-remove-btn");
+//     // button.classList.add("remove-btn");
+
+//     // button.setAttribute("id", id++);
+
+//     // icono.classList.add("fa-solid fa-xmark");
+
+//     //to show
+//     div_todo.appendChild(div_container);
+//     // div_todo.appendChild(div_boton);
+//     div_container.appendChild(parrafo);
+//     // button.appendChild(icono);
+//     // div_boton.appendChild(button);
+
+//     //add all to the box
+//     containerTwo.appendChild(div_todo);
+
+//     // input empty again
+//     word.value = "";
+// }
+
+
+
+// btnAdd.addEventListener("click", () => {
+//  if(word.value == "" || word.value.trim() == "") {
+//     window.alert("empty input");
+//  } else {
+//     addAll();
+//  }
+// });
+
---
+++ src/js/localStorage.js
@@ -0,0 +1,32 @@
+const addBtn = document.getElementById("add-btn");
+const word = document.getElementById("inp-word");
+const containerTwo = document.querySelector(".container-two");
+
+const storeWord = () => {
+
+    const inputWord = {
+        input_word: word.value
+    };
+
+    if(localStorage.getItem("inputWords") == null ) {
+        let arrange = [];
+        arrange.push(inputWord); 
+        localStorage.setItem("inputWords", JSON.stringify(arrange));
+
+    }else {
+        let getWords = JSON.parse(localStorage.getItem("inputWords"));
+        getWords.push(tarea);
+        localStorage.setItem("inputWords", JSON.stringify(getWords));
+    }
+
+    word.value = "";
+
+};
+
+addBtn.addEventListener("click", () => {
+    if(word.value == "" || word.value.trim() == "") {
+        window.alert("empty input, write something");
+    }else{
+        storeWord();
+    }
+});
